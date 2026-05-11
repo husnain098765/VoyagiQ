c80528be48129a5b5ec9a10b587a3ca9ea7ddf83
Husnain <husnaineditor098765@gmail.com>
2026-05-11 21:20:48 +0500

    fixed leaflet window undefined issue

--- app/map/page.js
+++ app/map/page.js
@@ -1,11 +1,36 @@
+// "use client";
+
+// import MapComponent from "@/Components/MapComponent";
+
+// export default function MapPage() {
+//   return (
+//     <section className="mt-6">
+//       <h1 className="text-2xl font-bold mb-4">🌍 Map View</h1>
+//       <MapComponent />
+//     </section>
+//   );
+// }
+
+
 "use client";
 
-import MapComponent from "@/Components/MapComponent";
+import dynamic from "next/dynamic";
+
+const MapComponent = dynamic(
+  () => import("@/Components/MapComponent"),
+  {
+    ssr: false,
+    loading: () => <p>Loading map...</p>,
+  }
+);
 
 export default function MapPage() {
   return (
     <section className="mt-6">
-      <h1 className="text-2xl font-bold mb-4">🌍 Map View</h1>
+      <h1 className="text-2xl font-bold mb-4">
+        🌍 Map View
+      </h1>
+
       <MapComponent />
     </section>
   );
